184a7a5fbc3d37d5220ef6c43e2ca519fca2dad3
Davydov Roman <rdfrontend1@gmail.com>
2025-10-19 10:55:06 +0300

    commit & push

---
+++ src/constants.js
@@ -0,0 +1,62 @@
+export const cardArray = [
+    {
+        id: 0,
+        title: 'Сукно белое',
+        price: '170 ₽',
+        adress: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png',
+        description: 'Продаю не спеша чистое сукно.'
+    },
+
+    {
+        id: 1,
+        title: 'Сукно серое',
+        price: '180 ₽',
+        adress: 'Воронеж, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png',
+        description: 'Продаю не спеша серое сукно.'
+    },
+
+    {
+        id: 2,
+        title: 'Ведро пластиковое',
+        price: '190 ₽',
+        adress: 'Пермь, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png',
+        description: 'Продаю не спеша отличное ведро.'
+    },
+
+    {
+        id: 3,
+        title: 'Ведро оцинкованое',
+        price: '200 ₽',
+        adress: 'Мурманск, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png',
+        description: 'Продаю не спеша надёжное ведро.'
+    },
+
+    {
+        id: 4,
+        title: 'Вафли Артек',
+        price: '210 ₽',
+        adress: 'Таганрог, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png',
+        description: 'Продаю не спеша вкусные вафли.'
+    },
+
+    {
+        id: 5,
+        title: 'Мыльные пузыри',
+        price: '2000 ₽',
+        adress: 'Москва, р-н Бибирево',
+        date: '10 июля 11:39',
+        img: '/image/card-img.png',
+        description: 'Продаю не спеша летучие пузыри.'
+    },
+
+]
